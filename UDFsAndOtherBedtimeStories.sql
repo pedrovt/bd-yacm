d1df30083a42271c132e83f7ad02ebd7ee62705f
Paulo Vasconcelos <paulobvasconcelos@gmail.com>
2019-06-04 14:49:24 +0100

    Trigger ON DELETE DOCUMENT
    and minor corrections on trigger ValarMorghulis

--- UDFsAndOtherBedtimeStories.sql
+++ UDFsAndOtherBedtimeStories.sql
@@ -108,6 +108,15 @@ AS
 	RETURN 0;
 GO
 
+GO
+CREATE TRIGGER DocumentRekt ON YACM.Document AFTER DELETE
+AS
+	DECLARE @id AS INT;
+	SELECT @id=id FROM deleted;
+	DELETE YACM.TextFile	WHERE YACM.TextFile.id=@id;
+	DELETE YACM.OtherFile	WHERE YACM.OtherFile.id=@id;
+GO
+
 
 
 
@@ -115,10 +124,12 @@ GO
 -- === USER ===
 -- ============
 -- Event on Delete
-CREATE TRIGGER ValarMorghulis ON YACM.[User] AFTER INSERT
+GO
+CREATE TRIGGER ValarMorghulis ON YACM.[User] AFTER DELETE
 AS
 	DECLARE @id AS INT;
-	SELECT @id=id FROM inserted;
+	SELECT @id=id FROM deleted;
 	DELETE YACM.Manager		WHERE YACM.Manager.id=@id;
 	DELETE YACM.Participant	WHERE YACM.Participant.id=@id;
 	DELETE YACM.Sponsor		WHERE YACM.Sponsor.id=@id;
+GO
